bbb04e6cb8efae78279862ee666a2b9689c0728b
TheConArtist <clrhoades1@gmail.com>
2019-07-03 17:04:07 -0400

    Fixed misc errors
    Added error checking to make sure the times given are in the proper format as well as fixed the issue where the bot would not send back the correct time. I also corrected the output times to be more readable.

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
-var Discord = require('discord.js')
+var Discord = require('discord.js');
 // var Discord = require('discord.io');
-var logger = require('winston')
-var auth = require('./auth.json')
+var logger = require('winston');
+var auth = require('./auth.json');
 
 // Configure logger settings
 logger.remove(logger.transports.Console);
@@ -10,7 +10,7 @@ logger.add(new logger.transports.Console,
     colorize: true
 });
 
-logger.level = 'debug'
+logger.level = 'debug';
 
 // Initialize Discord Bot
 // var bot = new Discord.Client({
@@ -18,24 +18,24 @@ logger.level = 'debug'
 //    autorun: true
 // });
 
-const bot = new Discord.Client()
+const bot = new Discord.Client();
 
 bot.on('ready', function (evt) {
-    logger.info('Connected')
-    logger.info('Logged in as: ')
-    logger.info(bot.username + ' - (' + bot.id + ')')
+    logger.info('Connected');
+    logger.info('Logged in as: ');
+    logger.info(bot.username + ' - (' + bot.id + ')');
 });
 
 bot.on('ready', () => {
     // List servers the bot is connected to
-    console.log("Servers:")
+    console.log("Servers:");
     bot.guilds.forEach((guild) => {
-        console.log(" - " + guild.name)
+        console.log(" - " + guild.name);
     })
 
 })
 
-bot.login(auth.token)
+bot.login(auth.token);
 
 
 let player = {
@@ -54,31 +54,33 @@ function playTimes() {
     this.initialized = false; 
 }; 
 
-const totalMinutes = 1440
+const maxMinutes = 1440;
 
 var currentInstance = new playTimes();
 
-var totalNumberOfPlayers = 0
+var totalNumberOfPlayers = 0;
 
-var timeMatrix
+var numOfUsersWhoCanPlay = 0;
 
-var serverMembers = []
+var timeMatrix;
 
-var numOfUsersWhoResponded = 0
+var serverMembers = [];
+
+var numOfUsersWhoResponded = 0; 
+
+var membersWhoHaveResponded = [];
 
 function initializeMatrix(userLength) {
-    timeMatrix = Array.from(Array(userLength), () => new Array(totalMinutes))
+    timeMatrix = Array.from(Array(userLength), () => new Array(maxMinutes));
 
     // This is definitely an efficiency bootleneck. It would be better to fill
     // the array with False above instead of leaving them empty. 
     for (let x = 0; x < userLength; x++) {
-        for(let y = 0; y < totalMinutes; y++) {
-            timeMatrix[x][y] = false
+        for(let y = 0; y < maxMinutes; y++) {
+            timeMatrix[x][y] = false;
         }
     }
 }
-
-const maxMinutes = 1440;
 
 function getUsers(recievedMessage) {
 
@@ -86,54 +88,66 @@ function getUsers(recievedMessage) {
     // This array will be used as a reference. In the matrix, the 
     // user at index i in memberIDs is the same user at index i in
     // the matrix. 
-    let keys = Array.from(recievedMessage.guild.members.keys())
+    let keys = Array.from(recievedMessage.guild.members.keys());
 
     for (let y = 0; y < keys.length; y++) {
         if(recievedMessage.guild.members.get(keys[y]).user.username != 'PlaytimeBot') {
-            serverMembers.push(recievedMessage.guild.members.get(keys[y]).user.username)
-            totalNumberOfPlayers++
+            serverMembers.push(recievedMessage.guild.members.get(keys[y]).user.username);
+            totalNumberOfPlayers++;
         }
     }
 
-    initializeMatrix(serverMembers.length)
+    numOfUsersWhoCanPlay = totalNumberOfPlayers;
+
+    initializeMatrix(serverMembers.length);
     console.log(serverMembers)
 };  
 
     // TODO
     // Convert the user to a value between 0 and maxUsers that's unique. 
     // Call this value x. 
+
+//var toAssignIndex = 0;
+
 function indexValue(user) {
-    return 0;
+    return numOfUsersWhoResponded;
 }
 
 function returnPlayTimes(channel) {
-    var recommendedPlayTimes = givePlayTimes()
-
-    //channel.send(recommendedPlayTimes)
+    var recommendedPlayTimes = givePlayTimes();
 
     if(recommendedPlayTimes.lastIndexOf(true) == -1) {
-        channel.send('It looks like there are no times where everyone can play :(')
+        channel.send('It looks like there are no times where everyone can play :(');
     } else {
-        var timesInRegularFormat = convertBackToTime(recommendedPlayTimes)
+        var timesInRegularFormat = convertBackToTime(recommendedPlayTimes);
 
-        channel.send('There are some possible playtimes!')
+        channel.send('There are some possible playtimes!');
 
-        channel.send(timesInRegularFormat)
+        channel.send(timesInRegularFormat);
     }
 }
 
 function convertMinutesToString(timeInMinutes) {
-    var extraInfo
-    var hours = Math.floor(timeInMinutes / 60)
-    var minutes = timeInMinutes % 60
+    var extraInfo;
+    var hours = Math.floor(timeInMinutes / 60);
+    var minutes = timeInMinutes % 60;
 
     if(timeInMinutes > 719 && timeInMinutes < 1381) {
-        extraInfo = 'pm'
+        extraInfo = 'pm';
+        hours = hours - 12;
     } else {
-        extraInfo = 'am'
+        extraInfo = 'am';
     }
 
-    return hours + ':' + minutes + extraInfo
+    if(hours === 0) {
+        hours = 12;
+    }
+
+    if(Math.floor(minutes/10) == 0) {
+        return hours + ':0' + minutes + extraInfo;
+    } else {
+        return hours + ':' + minutes + extraInfo;
+    }
 }
 
 // Converts the all minutes format array back to a readable format. 
@@ -160,6 +174,7 @@ function convertBackToTime(inMinutes) {
             finaltime = 0
         }
     }
+    return timeInRegularFormat
 }
 
 // Takes the timeMatrix and finds the common playtimes and stores them in an array
@@ -169,30 +184,24 @@ function givePlayTimes() {
 
     for(i = 0; i < maxMinutes; i++) {
         if(timeMatrix[0][i] == false) {
-            playTimes.push(false)
+            playTimes.push(false);
         } else {
-            var flag = true
+            let flag = true;
             for(j = 1; j < totalNumberOfPlayers && flag; j++) {
-                if(timeMatrix[j, i] == false) {
-                    playTimes.push(false) 
-                    // I don't know if that is valid
-                    flag = false
-                    // j = totalNumberOfPlayers
+                if(!timeMatrix[j][i]) {
+                    playTimes.push(false);
+                    flag = false;
+                    j--;
                 }
             }
 
             if(j == totalNumberOfPlayers) {
-                playTimes.push(true)
+                playTimes.push(true);
             }
-
-            // This isn't right :(
-            // if(playTimes[i] === 'uninitialized') {
-            //    playTimes.push(true)
-            // }
         }
     }
 
-    return playTimes
+    return playTimes;
 }
 
 // Inserts all of the time between and including time1 and time2 for user into
@@ -200,7 +209,7 @@ function givePlayTimes() {
 function intoMatrix(time1, time2, user) {
     var x = indexValue(user); 
 
-    for(let i = time1; i < time2; i ++) {
+    for(let i = time1; i <= time2; i ++) {
         timeMatrix[x][i] = true;
     } 
 }
@@ -211,7 +220,6 @@ function intoMatrix(time1, time2, user) {
 function transformPlaytime(times, user) {
     var time1, time2, extra1, extra2, intTime1, intTime2;
 
-    
         timeInterval = times;
         if(timeInterval.includes(':')) {
             timeInterval = timeInterval.replace(':', '');
@@ -224,18 +232,6 @@ function transformPlaytime(times, user) {
         timeInterval = timeInterval.split('-');
         time1 = timeInterval[0];
         time2 = timeInterval[1];
-        // This needs to be validated
-        /* time1 = timeInterval.split(0, '-');
-        time2 = timeInterval.split('-' + 1); */
-
-        logger.info('First time1 ' +  time1);
-        logger.info('First time2 ' +  time2);
-        
-        // extra1 = time1.splice(time1.length - 3, time1.length - 1);
-        // extra2 = time2.splice(time2.length - 3, time2.length - 1);
-
-        // time1 = time1.splice(time1.length - 3, time1.length - 1);
-        // time2 = time2.splice(time2.length - 3, time2.length - 1);
 
         extra1 = time1.slice(time1.length - 2, time1.length);
         extra2 = time2.slice(time2.length - 2, time2.length);
@@ -247,16 +243,15 @@ function transformPlaytime(times, user) {
         intTime2 = parseInt(time2, 10);
 
         if((extra1 == 'pm' || extra1 == 'PM') && intTime1 < 1200) {
-            intTime1 += 1200
+            intTime1 += 1200;
         }
 
         if((extra2 == 'pm' || extra2 == 'PM') && intTime2 < 1200) {
-            intTime2 += 1200
+            intTime2 += 1200;
         }
 
         if((extra1 == 'am' || extra1 == 'AM') && intTime1 >= 1200) {
             intTime1 = intTime1 % 100;
-            logger.info("HERE: " + intTime1);
         }
 
         if((extra2 == 'am' || extra2 == 'AM') && intTime2 >= 1200) {
@@ -270,9 +265,6 @@ function transformPlaytime(times, user) {
         intTime1 = (intTime1 % 100) + (Math.floor(intTime1 / 100) * 60); 
         intTime2 = (intTime2 % 100) + (Math.floor(intTime2 / 100) * 60);
 
-        logger.info('Final time1 ' + intTime1);
-        logger.info('Final time2 ' +  intTime2);
-
         intoMatrix(intTime1, intTime2, user);
 };
 
@@ -280,7 +272,8 @@ function extractTimes(args, user) {
     for(x = 1; x < args.length; x++) {
         transformPlaytime(args[x], user);
     }
-    console.log(timeMatrix)
+    // For troubleshooting
+    //console.log(timeMatrix)
 };
 
 /*
@@ -293,47 +286,34 @@ function extractTimes(args, user) {
     !timesup: Force the bot to return the results that have been given before the time
               limit has expired. 
 */
-// bot.on('message', function (user, userID, channelID, message, evt) {
-
 bot.on('message', (recievedMessage) => {
-    // Our bot needs to know if it will execute a command
-    // It will listen for messages that will start with `!`
 
     var message = recievedMessage.content;
 
     if (message.substring(0,1) == '!') {
-        var args = message.substring(1).split(' ')
-        var cmd = args[0]
+        var args = message.substring(1).split(' ');
+        var cmd = args[0];
        
-        //This uncommented out removed the first item in args, I think. I'm not sure,
-        //it was boilerplate code from a tutorial. 
-        //args = args.splice(1);
         switch(cmd) {
-            // !ping
-            // if args[1] is not empty, go through the arguments getting the names
-            // of all the users you want to include. 
             case 'playtime':
                 if(currentInstance.initialized == false) {
-                    // getUsers(channelID, bot.guilds);
 
-                    // Correctly returns TheConArtist
-                    // console.log(recievedMessage.guild.members.get('385889455317450761').user.username)
+                    getUsers(recievedMessage);
 
-                    getUsers(recievedMessage)//recievedMessage.channel.id, recievedMessage.guild.id, recievedMessage.channel)
+                    let x = recievedMessage.guild.members.random.toString;
 
-                    let x = recievedMessage.guild.members.random.toString
+                    recievedMessage.channel.send(recievedMessage.author + ' wants to play! What times can everyone join in today?\n\nResponse options:\n\n!no: This indicates that you cannot play today.\n!times: This means you are giving me the times you can play and then follow this command with the times you can play. You can enter them in this format:\n\n\"!times 12:30pm-2:00pm 4:00pm-5:30pm"');
 
-                    recievedMessage.channel.send(recievedMessage.author + ' wants to play! What times can everyone join in today?\n\nResponse options:\n\n!no: This indicates that you cannot play today.\n!times: This means you are giving me the times you can play and then follow this command with the times you can play. You can enter them in this format:\n\n\"!times 12:30pm-2:00pm 4:00pm-5:30pm"')
+                    var currentDate = new Date();
+                    var time = currentDate.getTime();
 
-                    var currentDate = new Date()
-                    var time = currentDate.getTime()
-
-                    currentInstance.initialized = true
+                    currentInstance.initialized = true;
 
                     //TODO
                     // If the number of players was represented in the arguments, put it 
                     // in the playtime instance. 
                     // args.length != 1
+                    /*
                     if(args.length != 1) {
                         if(!isNaN(args[1])) { 
                             currentInstance.players = args[1]
@@ -342,58 +322,69 @@ bot.on('message', (recievedMessage) => {
                             
                         }
                     } 
+                    */
                 } else {
-                    recievedMessage.channel.send('A playtime check has already started. You cannot start another one until the current one is closed.')
+                    recievedMessage.channel.send('A playtime check has already started. You cannot start another one until the current one is closed.');
                 }
 
             break;
 
             case 'no':
-                if (currentInstance.initialized == false) {
-                    recievedMessage.channel.send('I am sorry that you cannot join us ' + recievedMessage.author)
+                if (currentInstance.initialized) {
+                    recievedMessage.channel.send('I am sorry that you cannot join us ' + recievedMessage.author);
 
-                    //TODO
-                    //Indicate in currentInstance that user cannot play. Maybe leave their
-                    //times as false. 
+                    extractTimes(["","12:00am-11:59pm"], recievedMessage.author);
+                    membersWhoHaveResponded.push(recievedMessage.author);
+                    numOfUsersWhoResponded++;
 
-                    numOfUsersWhoResponded++
-                    // Maybe I can remove the person from the matrix..
-
+                    if(numOfUsersWhoResponded == totalNumberOfPlayers) {
+                        returnPlayTimes(recievedMessage.channel);
+    
+                        totalNumberOfPlayers = 0;
+                        numOfUsersWhoResponded = 0;
+                        currentInstance.initialized = false;
+                        serverMembers = [];
+                        membersWhoHaveResponded = [];
+                    }
 
                 } else {
-                    recievedMessage.channel.send('I am sorry that you cannot join us ' + recievedMessage.author)
+                    recievedMessage.channel.send('Invalid command. A session has not been initiated.');
                 }
             break;
 
             case 'forceStop':
                 if (currentInstance.initialized) {
-                    returnPlayTimes(recievedMessage.channel)
+                    returnPlayTimes(recievedMessage.channel);
 
-                    totalNumberOfPlayers = 0
-                    currentInstance.initialized = false
+                    totalNumberOfPlayers = 0;
+                    currentInstance.initialized = false;
                 } else {
-                    recievedMessage.channel.send('Sorry, but a playtime needs to be initiated before you can call that command.')
+                    recievedMessage.channel.send('Sorry, but a playtime needs to be initiated before you can call that command.');
                 }
             break;
 
             case 'times':
-                // TODO:
-                // Add a condition to check to make sure that the author hasn't already answered. 
-                extractTimes(args, recievedMessage.author)
-                recievedMessage.channel.send('Thank you, ' + recievedMessage.author + '! Your times have been recorded.')
-                numOfUsersWhoResponded++
-                if(numOfUsersWhoResponded == totalNumberOfPlayers) {
-                    returnPlayTimes(recievedMessage.channel)
-
-                    totalNumberOfPlayers = 0
-                    numOfUsersWhoResponded = 0
-                    currentInstance.initialized = false
-                    serverMembers = []
+                if(membersWhoHaveResponded.includes(recievedMessage.author) === false) {
+                    try {
+                        extractTimes(args, recievedMessage.author);
+                        recievedMessage.channel.send('Thank you, ' + recievedMessage.author + '! Your times have been recorded.');
+                        numOfUsersWhoResponded++;
+                        membersWhoHaveResponded.push(recievedMessage.author);
+                    } catch(err) {
+                        recievedMessage.channel.send(recievedMessage.author + ", your times are in the wrong format. Please put them in the format '12:00am-2:00am 6:00pm-9:00pm'");
+                    }
+                } else {
+                    recievedMessage.channel.send('I am sorry' + recievedMessage.author + ', but your answer has already been recorded.');
                 }
+                if(numOfUsersWhoResponded == totalNumberOfPlayers) {
+                    returnPlayTimes(recievedMessage.channel);
 
-                // recievedMessage.channel.send('totalNumberOfPlayers ' + totalNumberOfPlayers)
-                // recievedMessage.channel.send('numOfUsersWhoResponded ' + numOfUsersWhoResponded)
-
+                    totalNumberOfPlayers = 0;
+                    numOfUsersWhoResponded = 0;
+                    currentInstance.initialized = false;
+                    serverMembers = [];
+                    membersWhoHaveResponded = [];
+                }
             break; 
         }
     }
